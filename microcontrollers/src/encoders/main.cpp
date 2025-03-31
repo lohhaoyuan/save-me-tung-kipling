3efa52691ba4b3887088a69803a8f91fba067d1f
hao yuan <221409L@student.hci.edu.sg>
2025-03-31 21:10:25 +0800

    fuck encoders

--- microcontrollers/src/encoders/main.cpp
+++ microcontrollers/src/encoders/main.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 #include "stm32f4.h"
-#include "enc.h"
 PacketSerial TeensyPacketSerial;
 
 void setup(){
@@ -20,30 +19,10 @@ void setup(){
     TeensyPacketSerial.setPacketHandler(&TeensyPacketHandler);
 
 
-    for (uint8_t i = 0; i < 4; i++) {
-    pinMode(encoderPins[i][0], INPUT_PULLUP);
-    pinMode(encoderPins[i][1], INPUT_PULLUP); 
-    }
-
-    // Attach interrupts for each encoder pin
-    attachInterrupt(digitalPinToInterrupt(encoderPins[0][0]), encoder1ISR, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encoderPins[0][1]), encoder1ISR, CHANGE);
-
-    attachInterrupt(digitalPinToInterrupt(encoderPins[1][0]), encoder2ISR, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encoderPins[1][1]), encoder2ISR, CHANGE);
-
-    attachInterrupt(digitalPinToInterrupt(encoderPins[2][0]), encoder3ISR, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encoderPins[2][1]), encoder3ISR, CHANGE);
-
-    attachInterrupt(digitalPinToInterrupt(encoderPins[3][0]), encoder4ISR, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encoderPins[3][1]), encoder4ISR, CHANGE);
-
-
 }
 
 void loop(){
-    // updateRPM();
-    // delay(TIMER_INTERVAL);
+
     // TeensyPacketSerial.update();
     // drive(200,200,200,200);
     for (int i = 0; i < 256; i++) {
